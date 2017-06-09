7500096a9b0c5328e2be35b616ed9f4c0e39a739
mayankpi <mayankm522@gmail.com>
2017-06-09 17:33:43 +0530

    Video 8 complete- Blurring and Smoothing

--- video7.py
+++ video7.py
@@ -16,10 +16,23 @@ while True:
 
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('frame', frame)
+    kernel = np.ones((15, 15), np.float32)/225
+    smoothed = cv2.filter2D(result, -1, kernel)
+
+    gaussian = cv2.GaussianBlur(result, (15, 15), 0)
+
+    median = cv2.medianBlur(result, 15)
+
+    bilateral = cv2.bilateralFilter(result, 15, 75, 75)
+
+    cv2.imshow('gausssian', gaussian)
+    cv2.imshow('median', median)
+    cv2.imshow('bilateral', bilateral)
+    cv2.imshow('smoothed', smoothed)
+    # cv2.imshow('frame', frame)
     #cv2.imshow('hsv', hsv)
-    cv2.imshow('mask', mask)
-    cv2.imshow('result', result)
+    #cv2.imshow('mask', mask)
+    #cv2.imshow('result', result)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
